27fe3b7b7b7c77278744d8e5e7dfaf4ee4f003aa
Wang <yinan.wang@afconsult.com>
2023-02-21 21:07:46 +0800

    *feature-1 int input is beyond test
    This is the test for int border.

---
+++ Int_Max/Int_Max.c
@@ -0,0 +1,13 @@
+#include "stdio.h"
+#include "string.h"
+#include "stdbool.h" 
+
+int main()
+{
+	printf("test the MAX int!\n");
+	int test = 0;
+	scanf("please input the %d", &test);
+	printf("the input test is %d!\n", test);
+
+	return 0;
+}
